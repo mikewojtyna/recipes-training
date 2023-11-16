fa1f999a850c96173d60e7d75d3b7b02e76868bc
Mike Wojtyna <mike@wojtyna.pl>
2023-11-16 17:30:54 +0100

    Add nice objects problem

---
+++ src/main/java/pl/wojtyna/trainings/recipes/crowdsorcery/problems/niceobjects/Investor.java
@@ -0,0 +1,24 @@
+package pl.wojtyna.trainings.recipes.crowdsorcery.problems.niceobjects;
+
+public class Investor {
+
+    public void investInto(Project project) {
+        throw new UnsupportedOperationException("Implement this method");
+    }
+
+    public void investInto(Asset asset) {
+        throw new UnsupportedOperationException("Implement this method");
+    }
+
+    public void register(InvestorsRegistry investorsRegistry) {
+        throw new UnsupportedOperationException("Implement this method");
+    }
+
+    public void unregister(InvestorsRegistry investorsRegistry) {
+        throw new UnsupportedOperationException("Implement this method");
+    }
+
+    public void receiveInterests(Project project) {
+        throw new UnsupportedOperationException("Implement this method");
+    }
+}
