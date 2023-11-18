0150cab458b40e9785ad68f1f0dd221ba947a4a2
Mike Wojtyna <mike@wojtyna.pl>
2023-11-18 16:55:08 +0100

    Add knowledge example

---
+++ src/main/java/pl/wojtyna/trainings/recipes/crowdsorcery/problems/knowledge/AllowedInvestments.java
@@ -0,0 +1,6 @@
+package pl.wojtyna.trainings.recipes.crowdsorcery.problems.knowledge;
+
+public interface AllowedInvestments {
+
+    boolean isValid(Investor investor, Project project);
+}
---
+++ src/main/java/pl/wojtyna/trainings/recipes/crowdsorcery/problems/knowledge/Investor.java
@@ -0,0 +1,4 @@
+package pl.wojtyna.trainings.recipes.crowdsorcery.problems.knowledge;
+
+public record Investor() {
+}
